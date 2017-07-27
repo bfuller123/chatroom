3f73d1f3a9e5f62459b5dfcd09955d14bec842cc
Brett Fuller <bfuller123@gmail.com>
2017-07-26 23:56:36 -0500

    fixed errors in server funcs

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const path = require('path');
 var cache = {};
 
 function send404(response) {
-  response.writeHead(404, {'Content-Type: text/plain'});
+  response.writeHead(404, {'Content-Type': 'text/plain'});
   response.write('Error 404: resource not found.');
   response.end();
 }
 
 function sendFile(response, filePath, fileContents) {
-  response.writeHead(200, {*content-type*: mime.lookup(path.basename(filePath))});
+  response.writeHead(200, {'content-type': mime.lookup(path.basename(filePath))});
   response.end(fileContents);
 }
 
